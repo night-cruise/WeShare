aa9a6e0af0c8734ebf80fe02460123975334ace5
Night Cruising <2586447362@qq.com>
2021-02-21 21:24:50 +0800

    helper function.

---
+++ WeShare/helpers.py
@@ -0,0 +1,75 @@
+#!/user/bin/python
+# -*-coding:UTF-8-*-
+""" 
+@Author:    Night Cruising
+@File:      helpers.py
+@Time:      2021/02/18
+@Version:   3.7.3
+@Desc:      None
+"""
+# here put the import lib
+try:
+    from urlparse import urlparse, urljoin
+except ImportError:
+    from urllib.parse import urlparse, urljoin
+
+from flask import request, redirect, url_for, current_app
+from itsdangerous import BadSignature, SignatureExpired
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+
+from WeShare.settings import Operations
+from WeShare.models import User
+from WeShare.extensions import db
+
+
+def is_safe_url(target):
+    ref_url = urlparse(request.host_url)
+    test_url = urlparse(urljoin(request.host_url, target))
+    return test_url.scheme in ('http', 'https') and ref_url.netloc == test_url.netloc
+
+
+def redirect_back(default='main.index', **kwargs):
+    for target in request.args.get('next'), request.referrer:
+        if not target:
+            continue
+        if is_safe_url(target):
+            return redirect(target)
+    return redirect(url_for(default, **kwargs))
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in current_app.config['WESHARE_ALLOWED_IMAGE_EXTENSIONS']
+
+
+def generate_token(user, operation, expire_in=None, **kwargs):
+    s = Serializer(current_app.config['SECRET_KEY'], expire_in)
+    data = {'id': user.id, 'operation': operation}
+    data.update(**kwargs)
+    return s.dumps(data)
+
+def validate_token(user, token, operation, new_password=None):
+    s = Serializer(current_app.config['SECRET_KEY'])
+    try:
+        data = s.loads(token)
+    except (SignatureExpired, BadSignature):
+        return False
+
+    if operation != data.get('operation') or user.id != data.get('id'):
+        return False
+
+    if operation == Operations.CONFIRM:
+        user.confirmed = True
+    elif operation == Operations.RESET_PASSWORD:
+        user.set_password(new_password)
+    elif operation == Operations.CHANGE_EMAIL:
+        new_email = data.get('new_email')
+        if new_email is None:
+            return False
+        if User.query.filter_by(email=new_email).first() is not None:
+            return False
+        user.email = new_email
+    else:
+        return False
+    db.session.commit()
+    return True
