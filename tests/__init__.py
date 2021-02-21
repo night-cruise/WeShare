29bac1a6e6c50a7b7f13d239da513b33934a341f
Night Cruising <2586447362@qq.com>
2021-02-21 21:25:26 +0800

    test.

---
+++ tests/__init__.py
@@ -0,0 +1,10 @@
+#!/user/bin/python
+# -*-coding:UTF-8-*-
+""" 
+@Author:    Night Cruising
+@File:      __init__.py.py
+@Time:      2021/02/19
+@Version:   3.7.3
+@Desc:      None
+"""
+# here put the import lib
