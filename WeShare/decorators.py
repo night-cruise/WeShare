e39b57c8bdf2d87afc42aa3b0c42f9c0d864e80a
Night Cruising <2586447362@qq.com>
2021-02-21 21:27:45 +0800

    eamil

---
+++ WeShare/decorators.py
@@ -0,0 +1,10 @@
+#!/user/bin/python
+# -*-coding:UTF-8-*-
+""" 
+@Author:    Night Cruising
+@File:      decorators.py
+@Time:      2021/02/18
+@Version:   3.7.3
+@Desc:      None
+"""
+# here put the import lib
